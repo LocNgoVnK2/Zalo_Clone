6a8a49b096476f553c623ceeececfd898aa6ddc7
Leosaas <anhldt2001@gmail.com>
2023-06-19 11:34:25 +0700

    save temporarily to pull from main

---
+++ Infrastructure/Entities/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Entities
+{
+    [Table("MESSAGE_BASE")]
+    public class Message
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Column("userSrc")]
+        public string Sender { get; set; }
+        [Column("userDes")]
+        public string UserDesId { get; set;}
+        public DateTime BlockDate { get; set; }
+
+
+    }
+}
