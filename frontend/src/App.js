648e3336c3bd7cd7ab180b7d8654480f977b1326
LocNgo <ngophuocloc0309@gmail.com>
2023-07-09 14:39:20 +0700

    add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Login from './Components/Login';
-import Header from './Components/Header';
+
 class App extends Component {
   handleSelect = (eventKey) =>{ alert(`selected ${eventKey}`)};
 
